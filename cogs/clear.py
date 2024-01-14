73ecf85efc5c647cd660046b4e7d6d38a3eae833
Kamil Żegleń <kamyrdol32@gmail.com>
2024-01-15 00:09:41 +0100

    Update 0.2

--- cogs/clear.py
+++ cogs/clear.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -8,12 +10,19 @@ class Clear(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+
     @app_commands.command(name="clear")
     async def clear(self, interaction: discord.Interaction, amount: int):
+        await interaction.response.defer(ephemeral=True)
+
         channel = interaction.channel
 
-        await channel.purge(limit=amount)
-        await channel.send(f'Usunięto {amount} wiadomości!', delete_after=5)
+        # Pętla usunięcia z opóźnieniem
+        async for message in channel.history(limit=amount):
+            await asyncio.sleep(0.75)
+            await message.delete()
+
+        await interaction.followup.send(f'Usunięto {amount} wiadomości!')
 
 
     async def cog_load(self):
